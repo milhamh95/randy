1303a6c239853c366ac9c94b3ee4687ed60ca06f
Muhammad Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2024-02-15 19:57:13 +0700

    feat: initialize randyy

---
+++ lib/randy.ex
@@ -0,0 +1,18 @@
+defmodule Randy do
+  @moduledoc """
+  Documentation for `Randy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Randy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
